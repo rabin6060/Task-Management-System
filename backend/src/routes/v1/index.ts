4528af9e861103258fa3d661087a5d749293a9dd
Anand Mainali <anand.mainali@cyberarrow.io>
2024-06-09 19:15:39 +0545

    removed some unnecessary routes

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -4,7 +4,6 @@ import Health from './Health';
 import UserRouter from './Users';
 import AuthRouter from './Auth';
 import TaskRouter from './Task';
-import TagRouter from './Tag';
 import CommentsRouter from './Comment';
 import ActivityRouter from './Activity';
 
@@ -15,13 +14,8 @@ router.use('/auth', AuthRouter);
 router.use('/users', UserRouter);
 router.use('/activities', ActivityRouter);
 router.use('/tasks/:taskId/comments',CommentsRouter)
-router.use('/tags', TagRouter);
 router.use('/tasks', TaskRouter);
 
-/**
- * Import and add your routes here
- * Eg:
- *   router.use('/[route-name]', [Route]);
- */
+
 
 export default router;
